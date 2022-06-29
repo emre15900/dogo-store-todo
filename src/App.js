40fc93360bb5157021aed586a3de4e2f38cd824d
emre15900 <79264023+emre15900@users.noreply.github.com>
2022-06-29 16:17:02 +0300

    create Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Layout from "./components/Layout";
+import Header from "./components/Header";
 
 function App() {
   return <div className="App">
     <Layout>
+      <Header />
       <h1>Hello!</h1>
     </Layout>
   </div>;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="bg-primary text-white text-center py-2 px-3 rounded mb-3">
+        <h3>Todo App - Emre Sayın</h3>
+    </header>
+  )
+}
+
+export default Header
