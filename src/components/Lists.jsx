b9679ad9075e2433ca6ed37b054ae265ba20e283
emre15900 <79264023+emre15900@users.noreply.github.com>
2022-06-29 18:54:55 +0300

    create not found

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -2,19 +2,31 @@ import React from "react";
 import ListItem from "./ListItem";
 
 function Lists({ del, done, todos }) {
+    
+  if (todos.length <= 0) {
+    return (
+      <div className="card border-light p-5 text-center">
+        <h1 className="text-danger">No Todo Found!</h1>
+        <h4>Add your own todo!</h4>
+      </div>
+    );
+  }
+
   return (
     <div className="listWrap">
       <ul className="list-group">
-        {todos.map((todo) => (
-          <ListItem
-            key={todo.id}
-            id={todo.id}
-            title={todo.title}
-            delHandler={del}
-            doneHandler={done}
-            done={todo.done}
-          />
-        ))}
+        {todos &&
+          todos.map((todo) => (
+            <ListItem
+              key={todo.id}
+              id={todo.id}
+              title={todo.title}
+              delHandler={del}
+              doneHandler={done}
+              done={todo.done}
+            />
+          ))
+          }
       </ul>
     </div>
   );
